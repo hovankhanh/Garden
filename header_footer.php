a572d8df1df8bae74ff373be2dd7c88308d8e575
Ho Van Khanh <hokhanh246@gmail.com>
2017-12-06 03:28:13 +0700

    new file

---
+++ header_footer.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+
+    <link rel="stylesheet" type="text/css" href="../css/styleHeader.css"/>
+
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>      
+    <meta property="og:url" content="https://garden-1.bizwebvietnam.net">
+    <meta property="og:site_name" content="Garden">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Garden">
+    <meta property="og:image" content="http://bizweb.dktcdn.net/100/178/647/themes/233989/assets/logo.png?1511881369182">
+    <meta property="og:image:secure_url" content="https://bizweb.dktcdn.net/100/178/647/themes/233989/assets/logo.png?1511881369182">
+    <link href='//bizweb.dktcdn.net/100/178/647/themes/233989/assets/main.scss.css?1511881369182' rel='stylesheet' type='text/css' />
+    <link href='//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'/>
+
+</head>
+<body> 
+    <div id="container">       
+        <div id="header" class="header-3">
+            <div class="header-top">
+                <div class="wrapper">
+                    <div class="col-lg-7 col-md-7 col-sm-6 col-xs-12 hidden-xs">
+                        <div class="header-info">
+                            <div class="description-header hidden-sm">Cửa hàng thực phẩm sạch</div>
+                            <div class="color-primary"><a href="#">0123 4567 890</a>
+                            </div>
+                            <div class="color-primary hidden-xs">
+                                <a href="#">khanh.ho@student.paserellesnumeriques.org</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-5 col-md-5 col-sm-6 col-xs-12">
+                        <div class="pull-right left-acc clearfix">
+                            <div class="top-acc">
+                                <div class="dropdown">
+                                    <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tài khoản
+                                        <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dLabel">    
+                                        <li> <a href="#">Đăng nhập</a></li>
+                                        <li> <a href="#">Đăng ký</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="top-search">
+                                <form class="search-form form-inline">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" placeholder="Tìm kiếm..." name="query" />
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
+                                        </span>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="header-nav" id="navbar">
+
+                <div class="wrapper">
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6 hidden-sm hidden-xs">
+                        <a href="#" class="logo"></a>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-3 col-xs-3 cart-block-r">
+                        <div class="cart-block pull-right">
+                            <a href="#" class="a-icon-cart">
+                                <i class="flaticon-shopping-bag1 carti"></i>
+                                <span class="cart-number">0</span>
+                            </a>
+                            <div class="cart-box">
+                                <div class="cart-price top_cart_wrap pull-right">
+                                    <a href="#">
+                                        <span class="color-primary text-uppercase">Giỏ hàng</span>
+                                        <div class="items-cart"><span class="cart-number">0</span> SP</div>
+                                    </a>
+                                    <div class="cart-small">
+                                        <div class="top-cart-content" id="cart-info">
+                                            <ul id="cart-sidebar" class="mini-products-list"></ul>
+                                            <div class="fix-cart-mini cart-sidebar">
+                                                <div class="top-subtotal">
+                                                    <div class="price-total">
+                                                        <span class="label-price-total">Tổng tiền: </span>
+                                                    </div>
+                                                </div>
+                                                <div class="actions">
+                                                    <button class="btn-checkout btn" type="button">
+                                                        <span><i class="fa fa-check-square-o"></i> Thanh toán</span>
+                                                    </button>
+                                                    <button class="view-cart btn" type="button">
+                                                        <span><i class="fa fa-cart-arrow-down"></i> Giỏ hàng</span>
+                                                    </button>
+                                                </div> <!-- end actions -->
+                                            </div> <!-- end fix-cart-mini cart-sidebar -->
+                                        </div> <!--  end top-cart-content -->
+                                    </div> <!-- end cart-small -->
+                                </div> <!-- end cart-price top_cart_wrap pull-right -->
+                            </div> <!-- end cart-box -->
+                        </div> <!-- end cart-block pull-right -->
+                    </div> <!-- end col-lg-2 col-md-2 col-sm-3 col-xs-3 cart-block-r -->
+                
+
+                    <div class="col-lg-12 col-md-12 col-sm-6 col-xs-6 visible-sm visible-xs">
+                        <a href="#" class="logo-ipad">
+                            <img src="//bizweb.dktcdn.net/100/178/647/themes/233989/assets/logo.png?1511881369182" class="img-responsive center-block" alt="Garden"/>
+                        </a>
+                    </div>
+
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                      </button>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                       <ul class="nav navbar-nav">
+                            <li><a href="#">Trang chủ</a></li>
+                            <li><a href="#">Giới thiệu</a></li>
+                            <li>
+                                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Sản phẩm <span class="caret"></span></a>
+                                <ul class="dropdown-menu">                          
+                                    <li><a href="#">Đồ uống</a> </li>      
+                                    <li><a href="#">Gia vị nấu ăn</a></li>
+                                    <li><a href="#">Thực phẩm ăn liền</a></li>
+                                    <li><a href="#">Rau củ</a></li>
+                                    <li><a href="#">Trái cây</a></li>  
+                                </ul>
+                            </li> 
+                            <li><a href="#">Tin tức</a></li>
+                            <li><a href="#">Liên hệ</a></li>                      
+                            <li><a href="#">Hệ thống cửa hàng</a></li>    
+                        </ul>
+                    </div> <!-- end collapse navbar-collapse" id="myNavbar -->
+                </div> <!-- end wrapper -->
+            </div> <!-- end header-nav -->
+ 
+
+            <div id="slider">
+              <img class="mySlides" src="../image/slider_1.jpg">
+              <img class="mySlides" src="../image/slider_2.jpg">
+              <img class="mySlides" src="../image/slider_3.jpg">
+            </div>
+
+           <!--  chạy slide -->
+            <script>
+                window.onscroll = function() {myFunction()};
+
+                var navbar = document.getElementById("navbar");
+                var sticky = navbar.offsetTop;
+
+                function myFunction() {
+                  if (window.pageYOffset >= sticky) {
+                    navbar.classList.add("sticky")
+                  } else {
+                    navbar.classList.remove("sticky");
+                  }
+                }
+            </script>
+
+           <!--  thanh menu cố định -->
+            <script>
+                var myIndex = 0;
+                carousel();
+
+                function carousel() {
+                    var i;
+                    var x = document.getElementsByClassName("mySlides");
+                    for (i = 0; i < x.length; i++) {
+                       x[i].style.display = "none";  
+                    }
+                    myIndex++;
+                    if (myIndex > x.length) {myIndex = 1}    
+                    x[myIndex-1].style.display = "block";  
+                    setTimeout(carousel, 9000);    
+                }
+            </script>
+     
+        </div> <!-- end header -->
+
+
+
+
+  <footer id="footer">
+    <div class="footer-blocks">
+      <div class="wrapper">
+        <div class="row-footer">
+          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <div class="footer-logo">
+              <a href="/" class="logo-footer"></a>
+            </div>
+            <div class="f-b-box">
+              <p>"... Nơi tuyệt vời nhất để có được những giây phút vui và ý nghĩa bên người thân, nơi hoàn hảo nhất để ăn uống kết hợp học tập, làm việc và giải trí ..."</p>
+
+              <a class="btn btn-border dark" href="#"><span>Chi tiết</span></a>
+              
+              <div class="f-subscribe">
+                <h4>Đăng ký nhận tin</h4>
+                <div id="mc_embed_signup">
+                  <form class="validate">
+                    <div id="mc_embed_signup_scroll">
+                      <div class="mc-field-group">
+                        <input type="email" name="EMAIL" class="requireds email" id="mce-EMAIL" placeholder="Địa chỉ Email" />
+                      </div>
+                      <div id="mce-responses" class="clear">
+                        <div class="response" id="mce-error-response" style="display:none"></div>
+                        <div class="response" id="mce-success-response" style="display:none"></div>
+                      </div>
+      
+                      
+                      <div class="clear">
+                        <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"   class="btn btn-primary btn-icon-right"><span class="btn-icon"><i class="fa fa-envelope-o"></i></span> </button>
+                      </div>
+                    </div>
+                  </form>
+                </div> <!-- end mc_embed_signup -->
+              </div>  <!-- end f-subscribe -->       
+            </div> <!-- end f-b-box -->
+          </div> <!-- end col-lg-3 col-md-6 col-sm-6 col-xs-12 -->
+
+          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <h4 class="border">Sản phẩm</h4>
+            <div class="f-b-box">
+              <ul class="lat-tw ul-footer">               
+                <li><a href="#">Đồ uống</a></li>
+                
+                <li><a href="#">Gia vị nấu ăn</a></li>
+                
+                <li><a href="#">Thực phẩm ăn liền</a></li>
+                
+                <li><a href="#">Rau củ</a></li>
+                
+                <li><a href="#">Trái cây</a></li>               
+              </ul>
+            </div> <!-- end f-b-box -->
+          </div> <!-- end col-lg-3 col-md-6 col-sm-6 col-xs-12 -->
+
+          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <h4 class="border">Fanpage</h4>
+            <div class="f-b-box">
+              <div id="fb-root"></div>
+              <div class="fb-page"><a>Garden</a></div>
+            </div>
+          </div> <!-- end col-lg-3 col-md-6 col-sm-6 col-xs-12 -->
+
+          <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12 col-last-footer">
+            <h4 class="border">Thông tin liên hệ</h4>
+            <div class="f-b-box">
+              <div class="contact-f-wrapper">
+                <div class="f-contact-box">
+                  <span class="contact-name">
+                    Địa chỉ:
+                  </span>
+                  <span class="contact-info">
+                    175 Lý Thường Kiệt, Phường 6, Quận Tân Bình, TP. Hồ Chí Minh.
+                  </span>
+                </div>
+                <div class="f-contact-box">
+                  <span class="contact-name">
+                    VP 1:
+                  </span>
+                  <span class="contact-info">
+                    <a href="#">0123 456 789</a> / <a href="#">0123 456 789</a>
+                  </span>
+                </div> 
+
+                <div class="f-contact-box">
+                  <span class="contact-name">
+                    VP 2:
+                  </span>
+                  <span class="contact-info">
+                    <a href="#">0123 456 789</a> / <a href="#">0123 456 789</a>
+                  </span>
+                </div>
+      
+                <div class="f-contact-box">
+                  <span class="contact-name">
+                    Fax:
+                  </span>
+                  <span class="contact-info">
+                    <a href="#">0123 456 789</a>
+                  </span>
+                </div>
+                           
+                <div class="f-contact-box">
+                  <span class="contact-name">
+                    Email:
+                  </span>
+                  <span class="contact-info">
+                    <a href="#">khanh.ho@student.passellesnumeriques.org</a>
+                  </span>
+                </div>
+              </div> <!-- end contact-f-wrapper -->
+            </div> <!-- end f-b-box -->
+          </div> <!-- end col-lg-3 col-md-6 col-sm-6 col-xs-12 col-last-footer -->
+        </div> <!-- end row-footer -->
+      </div> <!-- end wrapper -->
+    </div> <!-- end footer-blocks -->
+
+    <div class="footer-bottom">
+      <div class="wrapper">
+        <div class="row-footer">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="copiright text-center">
+              © Bản quyền thuộc về Kiến Vàng <span class="hidden-480">|</span> 
+              <span class="power-by">Cung cấp bởi<a href="#">Bizweb</a>.</span>
+            </div>
+          </div>
+        </div>
+      </div> <!-- end wrapper -->
+    </div> <!-- end footer-bottom -->
+  </footer> <!-- end footer -->
+</body>
+</html>
+
+
+
+
+
+    
